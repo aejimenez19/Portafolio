27d40a2f2d9ccf037f0dad0c7d6063884423b3d4
aejimenez19 <aejimenez19@gmail.com>
2025-08-06 23:52:45 -0500

    feat: moderniza el navbar con nuevo diseño y estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { PrincipalComponent } from './components/principal/principal.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { IntroduccionComponent } from './components/introduccion/introduccion.component';
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { TitleComponent } from './components/utils/title/title.component';
@@ -11,7 +11,7 @@ import { ContacsComponent } from './components/contacs/contacs.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, PrincipalComponent, FooterComponent, IntroduccionComponent, ProjectsComponent,
+  imports: [RouterOutlet, NavComponent, HeroComponent, FooterComponent, IntroduccionComponent, ProjectsComponent,
     TitleComponent, ContacsComponent
   ],
   templateUrl: './app.component.html',
